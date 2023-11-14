38f045f92ac07d31452164b7d9075a0539b451ad
Hunter Arbeeny <144467896+harbeeny@users.noreply.github.com>
2023-11-13 20:56:35 -0500

    assets added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Footer, Blog, Possibility, Features, WhatGPT4, Header} from './containers';
+import { Cta, Brand, Navbar} from './components';
+import './App.css';
 
 const App = () => {
   return (
-    <div>
-        <h1>GPT-4</h1>
+    <div className='App'>
+        <div className='gradient__bg'>
+            <Navbar />
+            <Header />
+        </div>
+        <Brand />
+        <WhatGPT4/>
+        <Features/>
+        <Possibility/>
+        <Cta/>
+        <Blog/>
+        <Footer/>
     </div>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './navbar.css'
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+
+// BEM -> Block element Modifier
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='gpt4__navbar'>
+      <div className='gpt4__navbar-links'>
+        <div className='gpt4__navbar-links_logo'>
+          <img src={logo} alt='logo'/>
+        </div>
+      </div>
+    </div>
   )
 }
 
