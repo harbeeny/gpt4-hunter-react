8e7970e352432f7d89a8514a62bde8735a4dfb19
Hunter Arbeeny <144467896+harbeeny@users.noreply.github.com>
2023-11-14 19:06:00 -0500

    Features Section Finished
    Starting Possibilities section

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './brand.css'
-
+import { google, slack, dropbox, atlassian, shopify } from './imports'
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className='gpt4__brand section__padding'>
+      <div>
+        <img src={google} alt='google'/>
+      </div>
+      <div>
+        <img src={slack} alt='slack'/>
+      </div>
+      <div>
+        <img src={atlassian} alt='atlassian'/>
+      </div>
+      <div>
+        <img src={dropbox} alt='dropbox'/>
+      </div>
+      <div>
+        <img src={shopify} alt='shopify'/>
+      </div>
+    </div>
   )
 }
 
--- src/containers/whatGPT4/WhatGPT4.jsx
+++ src/containers/whatGPT4/WhatGPT4.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './whatGPT4.css'
+import { Feature } from '../../components'
 
 const WhatGPT4 = () => {
   return (
-    <div>WhatGPT4</div>
+    <div className='gpt4__whatgpt4 section__margin' id='wgpt4'>
+      <div className='gpt4__whatgpt4-feature'>
+        <Feature title='What is GPT-4?' text='We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.'/>
+      </div>
+      <div className='gpt4__whatgpt4-heading'>
+        <h1 className='gradient__text'>The possibilites are beyond your imagination</h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className='gpt4__whatgpt4-container'>
+        <Feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought."/>
+        <Feature title='Knowledgebase' text="We so opinion friends me message as delight. Whole front do of plate heard oh ought."/>
+        <Feature title='Education' text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"/>
+      </div>
+    </div>
   )
 }
 
