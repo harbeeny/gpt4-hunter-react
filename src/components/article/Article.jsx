dade046f5ae928acbb97cee81a61d29e865b6339
Hunter Arbeeny <144467896+harbeeny@users.noreply.github.com>
2023-11-14 22:54:48 -0500

    Blog image Error

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './article.css'
-const Article = () => {
-  return (
-    <div>Article</div>
-  )
-}
+
+const Article = ({imgUrl, date, title}) => (
+    <div className='gpt4__blog-container_article'>
+      <div className='gpt__blog-container_article-image'>
+        <img src={imgUrl} alt='blog_img'/>
+      </div>
+      <div className='gpt4__blog-container_article-content'>
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
+  );
 
 export default Article
--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
 import './footer.css'
+import logo from '../../assets/logo.svg'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='gpt4__footer section__padding'>
+      <div className='gpt4_footer-heading'>
+        <h1 className='gradient__text'>Do you want to step into the future?</h1>
+      </div>
+      <div className='gpt4__footer-btn'>
+        <p>Request Early Access</p>
+      </div>
+      <div className='gpt4__footer-links'>
+        <div className='gpt4__footer-links_logo'>
+          <img src={logo} alt='logo' />
+          <p>Location</p>
+        </div>
+        <div className='gpt4__footer-links_div'>
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Socials Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className='gpt4__footer-links_div'>
+          <h4>Company</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className='gpt4__footer-links_div'>
+          <h4>Get in touch</h4>
+          <p>Location</p>
+          <p>085-123475</p>
+          <p>info@email.net</p>
+        </div>
+      </div>
+
+      <div className='gpt4__footer-copyright'>
+        <p>© 2023 GPT-4. All rights reserved.</p>
+      </div>
+    </div>
   )
 }
 
