1240bd27ad9748413e89136fbe05e4fb396c13b4
Hunter Arbeeny <144467896+harbeeny@users.noreply.github.com>
2023-12-04 20:12:54 -0500

    Update Blog.jsx

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -16,7 +16,7 @@ const Blog = () => (
       <div className="gpt4__blog-container_groupB">
         <Article imgUrl={blog02} date="Aug 12, 2019" title="GPT-4 and Open  AI is the future. Let us exlore" />
         <Article imgUrl={blog03} date="Sep 26, 2020" title="GPT-4 and Open  AI is the future. Let us exlore how it is?" />
-        <Article imgUrl={blog04} date="Sep 26, 2021" title="GPT-4 and Open  AI is the future. Let us exlore how it is?" />
+        <Article imgUrl={blog04} date="May 7, 2021" title="GPT-4 and Open  AI is the future. Let us exlore how it is?" />
         <Article imgUrl={blog05} date="Sep 26, 2023" title="GPT-4 and Open  AI is the future. Let us exlore how it is?" />
       </div>
     </div>
